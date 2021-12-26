6999d1e912186e571e8ca3ec13304304ed9112fc
Zübeyir Bodur <zubeyir99@hotmail.com>
2021-12-26 20:02:14 +0300

    added the connection string in appsetting.Development.json and commented it, just in case.

--- TravelAgencyEntity/TravelAgencyContext.cs
+++ TravelAgencyEntity/TravelAgencyContext.cs
@@ -49,6 +49,7 @@ namespace TravelAgencyEntity
             if (!optionsBuilder.IsConfigured)
             {
                 optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TravelAgency;Integrated Security=True;MultipleActiveResultSets=True;");
+                //optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Database=TravelAgency;Trusted_Connection=True;");
             }
         }
 
