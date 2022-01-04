c3ba4874211428f1732b6ab898bb58fb50ed5c43
Zübeyir Bodur <zubeyir99@hotmail.com>
2022-01-04 04:10:38 +0300

    reviewed tourswithout guide

--- TravelAgencyAPI/Controllers/GuidingController.cs
+++ TravelAgencyAPI/Controllers/GuidingController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -25,8 +26,63 @@ namespace TravelAgencyAPI.Controllers
         /// </summary>
         /// <param name="userInfo"></param>
         /// <returns></returns>
-        [HttpGet("action")] // Please set the http request type, and http request name.
-        public IActionResult Action()
+        [HttpGet("toursWithoutGuide")]
+        public IActionResult ToursWithoutGuide()
+        {
+            ResponseModel response = new ResponseModel();
+            try
+            {
+
+                string finalQuery = "SELECT tour_id, city, tour_name, tour_start_date, tour_description, price, tour_end_date, percents " +
+                    "FROM ( " +
+                    "(SELECT * " +
+                    "FROM Tour) EXCEPT " +
+                    "(SELECT Tour.tour_id, city, tour_name, tour_start_date, tour_description, price, discount_id,tour_end_date, discount_start_date " +
+                    "FROM Tour JOIN assign_guide ON Tour.tour_id=assign_guide.tour_id " +
+                    "WHERE assign_guide.assign_status ='ACCEPTED') " +
+                    ") as t1 LEFT JOIN Discount ON t1.discount_id=Discount.discount_id;";
+                Func<DbDataReader, TourDTO> map = x => new TourDTO
+                {
+                    tourId = (int)x[0],
+                    tourName = (string)x[2],
+                    city = (string)x[1],
+                    tourStartDate = (DateTime)x[3],
+                    tourEndDate = (DateTime)x[6],
+                    tourDescription = (string)x[4],
+                    price = (decimal)x[5],
+                    discountPercents = (x[7] != DBNull.Value)?((int)x[7]):0 // if percents is null, then the discount applied is zero percent
+                };
+                //var tours = dbContext.Tours.FromSqlRaw(finalQuery).ToList();
+                Console.Write(finalQuery);
+                var toursDTO = Helper.RawSqlQuery<TourDTO>(finalQuery, map);
+                // Get the discount of each tour
+                // Send an HTTP response as data, if necessary
+                response.Data = toursDTO;
+            }
+            catch (Exception ex)
+            {
+                // Catch SQL Exceptions, and send them to frontend
+                response.HasError = true;
+                response.ErrorMessage = ex.Message;
+                if (ex.InnerException != null)
+                {
+                    response.ErrorMessage += ": " + ex.InnerException.Message;
+                }
+            }
+            // Return the HTTP response
+            return Ok(response);
+        }
+
+
+
+        /// <summary>
+        /// Select a tour and assign an available guide. (employee)
+        /// </summary>
+        /// <param name="tourId"></param>
+        /// <param name="guideUId"></param>
+        /// <returns></returns>
+        [HttpGet("assignGuideForTour")]
+        public IActionResult AssignGuideForTour(int tourId, int guideUId, int agentUId)
         {
             ResponseModel response = new ResponseModel();
             try
